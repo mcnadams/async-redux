770267bd828fd895d9d6803c0d8e4de32e017d9e
mcnadams <mcneilbo@oregonstate.edu>
2019-05-29 14:20:25 -0700

    pretty useless middleware now exists

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import { 
+  createStore,
+  applyMiddleware
+} from 'redux';
 
-render(
-  <App />,
-  document.getElementById('root')
+
+const reducer = (state = {}, action) => {
+  return action;
+};
+
+const logger = store => next => action => {
+  console.log('start middleware action', action);
+  console.log('start middleware state', store.getState());
+  next(action);
+  console.log('end middleware action', action);
+  console.log('end middleware state', store.getState());
+};
+
+const store = createStore(
+  reducer,
+  applyMiddleware(
+    logger
+  )
 );
+
+store.dispatch({ type: 'hi' });
