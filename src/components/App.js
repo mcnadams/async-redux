8a62c9386fc1ce1e35283ccee27248070a8b2b25
mcnadams <mcneilbo@oregonstate.edu>
2019-05-30 14:52:30 -0700

    link

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import AllCharacters from '../containers/AllCharacters';
+import CharacterDetail from '../containers/CharacterDetail';
 
 export default function App() {
-  return <AllCharacters />;
+  return (
+    <Router>
+      <Link to='/'>Home</Link>
+      <Switch>
+        <Route exact path='/' component={AllCharacters} />
+        <Route path='/:id' component={CharacterDetail} />
+      </Switch>
+    </Router>
+  );
 }
